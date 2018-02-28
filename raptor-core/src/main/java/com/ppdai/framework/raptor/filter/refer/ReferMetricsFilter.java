8717b44d4d637f7922444e41f8f5defb07bf0114
yinzuolong <yinzuolong@ppdai.com>
2018-02-28 11:29:56 +0800

    修改filter

--- raptor-core/src/main/java/com/ppdai/framework/raptor/filter/refer/ReferMetricsFilter.java
+++ raptor-core/src/main/java/com/ppdai/framework/raptor/filter/refer/ReferMetricsFilter.java
@@ -1,14 +1,20 @@
 package com.ppdai.framework.raptor.filter.refer;
 
+import com.ppdai.framework.raptor.filter.AbstractFilter;
 import com.ppdai.framework.raptor.refer.Refer;
 import com.ppdai.framework.raptor.rpc.Request;
 import com.ppdai.framework.raptor.rpc.Response;
 
-public class ReferMetricsFilter extends AbstractReferFilter {
+public class ReferMetricsFilter extends AbstractFilter implements ReferFilter{
+
 
     @Override
     public Response filter(Refer<?> refer, Request request) {
-        return refer.call(request);
+        return null;
     }
 
+    @Override
+    public int getOrder() {
+        return 0;
+    }
 }
